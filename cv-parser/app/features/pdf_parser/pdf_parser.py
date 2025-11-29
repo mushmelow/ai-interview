aae0f738a95baada841740e0267ea8f0ba610ad0
Amir-Mojtahedi <amirrezamojtahedi2@gmail.com>
2025-11-28 21:35:08 -0500

    Add pdf parser

---
+++ cv-parser/app/features/pdf_parser/pdf_parser.py
@@ -0,0 +1,35 @@
+from typing import BinaryIO
+import pdfplumber
+import logging
+
+class PDFParser:
+    """A utility class for parsing text from PDF files."""
+
+    @staticmethod
+    def parse_pdf(pdf_file: BinaryIO) -> dict:
+        """
+        Parses a PDF file and extracts the text content.
+
+        This method reads a PDF file page by page, extracts the text from each
+        page, and concatenates it into a single string.
+
+        Args:
+            pdf_file (BinaryIO): The PDF file object to be parsed.
+
+        Returns:
+            dict: A dictionary where each key is a page number (e.g., 'page_1') and each value is the extracted text from that page. Returns an empty dictionary if an error occurs.
+        """
+        try:
+            text = ''
+            text_dict = dict()
+            with pdfplumber.open(pdf_file) as pdf:
+                for page in pdf.pages:
+                    page_text = page.extract_text()
+                    if page_text:
+                        text_dict[f'page_{page.page_number}'] = page_text
+                        text += page_text + '\n'
+            text_dict['full_text'] = text
+            return text_dict
+        except Exception as e:
+            logging.error(f"Error parsing PDF file: {e}")
+            return {}
--- cv-parser/app/main.py
+++ cv-parser/app/main.py
@@ -1,10 +1,12 @@
 # app/main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel
 from features.llm_client.llm_client import LLMClient
+from features.pdf_parser.pdf_parser import PDFParser
 
 app = FastAPI()
 llm = LLMClient()
+pdf_parser = PDFParser()
 
 class ChatRequest(BaseModel):
     question: str
@@ -14,7 +16,12 @@ async def ask_llm(body: ChatRequest):
     answer = llm.chat(body.question)
     return {"answer": answer}
 
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    file_content = PDFParser.parse_pdf(file.file)
+    return {"filename": file.filename, "content_type": file.content_type, "content": file_content}
+
+
 @app.get("/health")
 def health_check():
-    return {"message": "Docker + Fast API is runningg!"}
-
+    return {"message": "Docker + Fast API is running!"}
